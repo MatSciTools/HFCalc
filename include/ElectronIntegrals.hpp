e7e2fb74055a50b1fc5d1b5d2c53f21838bf94bf
Vishnu Raghuraman <vishnu.raman@gmail.com>
2022-08-26 23:06:14 -0400

    Header inclusion doubling fix

--- include/ElectronIntegrals.hpp
+++ include/ElectronIntegrals.hpp
@@ -3,16 +3,18 @@
 #include <string>
 #include "MatrixElement.hpp"
 #include "Molecule.hpp"
-#include "MultiProc.hpp"
+#include "OutputHandler.hpp"
 #include <Eigen/Dense>
 
 class ElectronIntegrals{
 public:
-    int matsize;
+    int matsize; 
+    std::vector <int> localmatsize;
     Eigen::MatrixXd S, T, V, g;
     Molecule mol;
     ElectronIntegrals(){};
     void Init(Molecule m);
+    void divideMatrixAmongRanks();
     void calSingleElectronMatrices();
     void calElectronRepulsionMatrix();
 };
