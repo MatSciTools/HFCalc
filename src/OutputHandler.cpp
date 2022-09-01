655cef2c95932977e9e8dc2477eb218c3f2f8aff
Vishnu Raghuraman <vishnu.raman@gmail.com>
2022-09-01 18:44:09 -0400

    Updated version

--- src/OutputHandler.cpp
+++ src/OutputHandler.cpp
@@ -21,7 +21,7 @@ void OutputHandler::Init(std::string ofile){
      writeString("***       ***    ***           **********   ***         ***   *********  **********");
      writeString("                                                                                   ");
      writeString("###################################################################################");
-     writeString("Version 1.0 (July 2022)");
+     writeString("Version 1.1 (September 2022)");
      writeString("###################################################################################");
      writeStringInt("Number of MPI Ranks: ", MultiProc::getTotalRanks());
      std::cout << "###################################################################################" << std::endl;
@@ -36,7 +36,7 @@ void OutputHandler::Init(std::string ofile){
      std::cout << "***       ***    ***           **********   ***         ***   *********  **********" << std::endl;
      std::cout << "                                                                                   " << std::endl;
      std::cout << "###################################################################################" << std::endl;
-     std::cout << "Version 1.0 (July 2022)" << std::endl;
+     std::cout << "Version 1.1 (September 2022)" << std::endl;
      std::cout << "###################################################################################" << std::endl;
      std::cout << "Using " << MultiProc::getTotalRanks() << " MPI Ranks" << std::endl;
     }
